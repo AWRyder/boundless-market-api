bf16cf90a930b0c33305a394f4469f3d1632175c
João Braz <joaofgbraz@gmail.com>
2019-11-14 09:48:07 +0000

    Initial skeleton created.

---
+++ src/main/java/net/wyvernia/boundlessmarketapi/BoundlessmarketapiApplication.java
@@ -0,0 +1,13 @@
+package net.wyvernia.boundlessmarketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoundlessmarketapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoundlessmarketapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/wyvernia/boundlessmarketapi/BoundlessmarketapiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wyvernia.boundlessmarketapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoundlessmarketapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
